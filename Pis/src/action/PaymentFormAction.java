d821cace0ef1a7bb26822fe29260a507a48cb8df
dayeon <dayeon@DESKTOP-9H7ARPI>
2016-08-09 13:48:13 +0900

    adf

---
+++ Pis/src/action/PaymentFormAction.java
@@ -0,0 +1,15 @@
+package action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import controller.CommandAction;
+
+public class PaymentFormAction implements CommandAction {
+	@Override
+	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+		// TODO Auto-generated method stub
+		return "/pointcharge/paymentForm.jsp";
+	}
+
+}
