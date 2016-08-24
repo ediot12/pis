25b1f52c3c5fc41c59c3788f7cd40f8f92aba51d
dayeon <dayeon@DESKTOP-9H7ARPI>
2016-08-24 13:50:31 +0900

    dddd

--- Pis/WebContent/park/payAfter.jsp
+++ Pis/WebContent/park/payAfter.jsp
@@ -1,12 +1,5 @@
 <%@ page contentType="text/html; charset=UTF-8"%>
-<html>
-<head>
-<title></title>
-</head>
-<body>
-<center>
-<table>
-ㅊㅋㅊㅋ
-</table>
-</body>
-</html>
+<script>
+alert("예약이 완료되었습니다.\n예약확인페이지로 이동합니다.");
+location.href="/Pis/park/myReserv.do";
+</script>
--- Pis/src/Reservation/DeleteReservAction.java
+++ Pis/src/Reservation/DeleteReservAction.java
@@ -1,14 +1,17 @@
 package Reservation;
  
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.text.SimpleDateFormat;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import Pay.DeleteDate;
 import controller.CommandAction;
 
 public class DeleteReservAction	implements CommandAction {
@@ -30,6 +33,9 @@ public class DeleteReservAction	implements CommandAction {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
+	
+		
+		
 		
 		try{
 			conn = getConnection();
@@ -47,7 +53,6 @@ public class DeleteReservAction	implements CommandAction {
 				
 //				*** test 1단계 ***
 				System.out.println("reservpark테이블에서 레코드 삭제");
-				
 				if(rs.next()){
 					pstmt = conn.prepareStatement("delete from pointlist where id='" + id +"'and use_point=? and parkname=?");
 					pstmt.setInt(1, use_point);
@@ -62,26 +67,44 @@ public class DeleteReservAction	implements CommandAction {
 						rs = pstmt.executeQuery();
 						
 //						*** test 3단계 ***
-						System.out.println("3");
+						System.out.println("3단계");
 						
 						if(rs.next()){
+							
 							int point = rs.getInt("point");
 							int before_point = use_point + point;
+							SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+							rs.getString("pdate");
+							String date = sdf.format(date1);
 							
-							System.out.println(point);
+							
+							System.out.println("3단계 이후 ::: " + point);
 							pstmt = conn.prepareStatement("update pointment set point=?, use_point=? where id='"+id+"'");
 							pstmt.setInt(1, before_point);
 							pstmt.setInt(2, 0);
 							pstmt.executeUpdate();
-							
-//							*** test 4단계 ***
 							System.out.println("pointment에 update완료 ");
-		
+							
+							
+							pstmt = conn.prepareStatement("SELECT parking_code,capacity2 FROM FIRSTDATE WHERE addr in (SELECT substr(parkloca,7)  FROM reservpark where id='?'");
+							pstmt.setString(1, id);
+							rs = pstmt.executeQuery();
+							
+//							*** test ***
+							System.out.println("select parking_code!");
+							
+							if(rs.next()){
+								
+							DeleteDate del = DeleteDate.getInstance();
+							del.FindDate(date, rs.getInt("capacity2"), rs.getInt("parking_code"));
+							
+							System.out.println("test 완료");
+							
+							}
 						}
-					}					
+					}
 				}
-			}
-
+			}					
 		}catch(Exception e){
 			e.printStackTrace();
 		}finally{
