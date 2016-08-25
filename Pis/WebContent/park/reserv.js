6f10d5d70a120947f624db7674a5eb683b98495a
ChangBaeKim <ChangBaeKim@ChangBaeKim-PC>
2016-08-25 13:34:55 +0900

    dd

--- Pis/WebContent/park/reserv.js
+++ Pis/WebContent/park/reserv.js
@@ -1,4 +1,12 @@
 function checkPay() {
+
+
+	
+	if(document.insert.calendar1.value==""){
+		alert("날짜를 입력해주세요.");
+		return false;
+	}
+	
 	if (confirm('확인을 누르시면 포인트가 차감되며 예약됩니다. 진행하시겠습니까?')) {
 
 	} else {
